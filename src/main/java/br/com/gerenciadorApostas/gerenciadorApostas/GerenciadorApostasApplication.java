7706e371b9f7f55b49b5307f0022d025f52acb2d
juniormendes <juniormendesjp@gmail.com>
2024-10-27 15:29:25 -0300

    Commits iniciais

---
+++ src/main/java/br/com/gerenciadorApostas/gerenciadorApostas/GerenciadorApostasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorApostas.gerenciadorApostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorApostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorApostasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerenciadorApostas/gerenciadorApostas/GerenciadorApostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorApostas.gerenciadorApostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
